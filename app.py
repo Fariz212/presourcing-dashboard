819e501207f922b3f67f8ec7a076daa7d0ef8d25
Farizky Ramadhan <fzk@Farizkys-MacBook-Air.local>
2026-09-15 10:24:43 +0700

    Fix host to 0.0.0.0 for AWS

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def save_data():
 
 if __name__ == '__main__':
     # Jalankan server di port 5000
-    app.run(debug=True, host='0.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
