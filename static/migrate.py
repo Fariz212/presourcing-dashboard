79fe6fd1b4910dfd117ac57cfcacce02cdb1bb5f
Farizky Ramadhan <fzk@Farizkys-MacBook-Air.local>
2026-09-21 14:29:33 +0700

    fix bug migrate

--- static/migrate.py
+++ static/migrate.py
@@ -3,8 +3,8 @@ import shutil
 import os
 
 # Nama database sudah disesuaikan
-DB_FILE = 'presourcing_db.sqlite' 
-BACKUP_FILE = 'presourcing_db_backup.sqlite'
+DB_FILE = 'presourcing_db' 
+BACKUP_FILE = 'presourcing_db_backup'
 
 def migrate_database():
     # 1. PROSES BACKUP
